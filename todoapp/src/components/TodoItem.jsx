372058dba832492e70bb8696df5e58d45bb9e151
Ameya Potdar <ameyapradippotdar@gmail.com>
2023-10-31 22:28:10 +0530

    created todoapp

---
+++ todoapp/src/components/TodoItem.jsx
@@ -0,0 +1,30 @@
+import styles from "./todoitem.module.css";
+
+export default function TodoItem({ item ,todos,setTodos}) {
+
+    function handleDelete(item){
+        console.log("Item to be clicked",item);
+        setTodos(todos.filter((todo)=>todo!==item));
+    }
+
+    function handleClick(name){
+      const newArray = todos.map((todo) => todo.name === name ? {...todo,done:!todo.done}:todo);
+      setTodos(newArray);
+    }
+
+    const todostatus = item.done?styles.completed : "";
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        <span className={todostatus} onClick={()=>handleClick(item.name)}>
+        {item.name}
+        </span>
+        <span>
+          <button onClick={()=> handleDelete(item)} className={styles.deleteButton}>x</button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
+}
